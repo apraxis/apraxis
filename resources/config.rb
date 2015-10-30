8465403638c17a65b595024f739a9edc81f4ccb3
Alex Redington and Joe Smith <pair+ar+js@cognitect.com>
2015-10-30 09:48:19 -0400

    Fix Gemfile and config.rb

--- resources/config.rb
+++ resources/config.rb
@@ -11,28 +11,29 @@ set :images_dir, 'images'
 
 set :layouts_dir, 'structure/layouts'
 
+activate :syntax, line_numbers: true
+
 compass_config do |config|
   require 'breakpoint'
   config.output_style = :compact
 end
 
-activate :directory_indexes
-
-activate :syntax, line_numbers: true
-
 set :markdown_engine, :kramdown
 
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-Dir["source/structure/components/**/_*.haml"].each do |raw_name|
+activate :directory_indexes
+
+live {
+  Dir["source/structure/components/**/_*.haml"]
+}.each do |raw_name|
   partial_name = raw_name.sub(/source\//,"").sub(/_(.*)\.haml/,'\1')
-  component_name = raw_name.gsub(/source\/(.*)\/_(.*).haml/,'/\1/\2.html')
+  component_name = raw_name.gsub(/source\/(.*)\/_(.*).haml/,'/\1/\2/index.html')
   proxy component_name, "/component_template.html", :locals => { :partial_name => partial_name }, :ignore => true
 end
 
 configure :build do
   activate :minify_css
-  activate :minify_javascript
-  activate :gzip
   ignore 'structure/layouts*'
+  ignore "/component_template.html"
 end
